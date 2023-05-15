e5ce7f6464ae8b7c62431d40274235c951828731
Ricardo Herrera <72674229+ricardoherreral@users.noreply.github.com>
2023-05-15 13:39:17 -0600

    Dart Maps

---
+++ Dart Programming Fundamentals/DartMaps.dart
@@ -0,0 +1,11 @@
+void main() {
+  var cars = {"Tesla": "Electro", "Toyota": "Gasoline"};
+  print(cars);
+
+  var fruits = new Map();
+  fruits["Apple"] = "Red";
+  print(fruits);
+
+  var userAges = {'Jeff': 22, "George": 31};
+  print(userAges["George"]);
+}
