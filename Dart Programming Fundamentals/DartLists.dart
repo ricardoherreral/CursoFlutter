e1836141c5f52a39f28297ccf67b339dbee8e20a
Ricardo Herrera <72674229+ricardoherreral@users.noreply.github.com>
2023-05-15 13:47:19 -0600

    Dart Lists

---
+++ Dart Programming Fundamentals/DartLists.dart
@@ -0,0 +1,13 @@
+void main() {
+  var list1 = ["a", "b", "c"];
+  print(list1);
+  print(list1[0]);
+  print(list1[1]);
+
+  list1.add("d");
+  print(list1);
+  print(list1.first);
+  print(list1.last);
+  print(list1.isEmpty);
+  print(list1.length);
+}
