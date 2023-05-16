fd2dc02a01a10cfb88704f6e322923123c18fa74
Ricardo Herrera <72674229+ricardoherreral@users.noreply.github.com>
2023-05-16 17:40:12 -0600

    App Taskly creada con AppBar y su texto

---
+++ taskly/lib/pages/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage();
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageStateClass();
+  }
+}
+
+class _HomePageStateClass extends State<HomePage> {
+  late double _deviceHeight, _deviceWidth;
+
+  _HomePageStateClass();
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _deviceHeight * 0.15,
+        title: const Text(
+          "Taskly",
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
