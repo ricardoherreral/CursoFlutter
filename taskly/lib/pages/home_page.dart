8c9769afa850cb1d6318c26eca1901e1f1555040
Ricardo Herrera <72674229+ricardoherreral@users.noreply.github.com>
2023-05-16 18:54:56 -0600

    Se creo la alerta para agregar una nueva tarea y se aplica el setState()

--- taskly/lib/pages/home_page.dart
+++ taskly/lib/pages/home_page.dart
@@ -12,12 +12,17 @@ class HomePage extends StatefulWidget {
 class _HomePageStateClass extends State<HomePage> {
   late double _deviceHeight, _deviceWidth;
 
+  //Por esto es clase Stateful
+  String? _newTaskContent;
+
   _HomePageStateClass();
 
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
+
+    print("Input Value: $_newTaskContent");
 
     return Scaffold(
       appBar: AppBar(
@@ -34,31 +39,57 @@ class _HomePageStateClass extends State<HomePage> {
       floatingActionButton: _addTaskButton(),
     );
   }
-}
 
-Widget _tasksList() {
-  return ListView(
-    children: [
-      ListTile(
-        title: const Text(
-          "Do Laundry!",
-          style: TextStyle(
-            decoration: TextDecoration.lineThrough,
+  Widget _tasksList() {
+    return ListView(
+      children: [
+        ListTile(
+          title: const Text(
+            "Do Laundry!",
+            style: TextStyle(
+              decoration: TextDecoration.lineThrough,
+            ),
           ),
-        ),
-        subtitle: Text(DateTime.now().toString()),
-        trailing: const Icon(
-          Icons.check_box_outlined,
-          color: Colors.red,
-        ),
-      )
-    ],
-  );
-}
+          subtitle: Text(DateTime.now().toString()),
+          trailing: const Icon(
+            Icons.check_box_outlined,
+            color: Colors.red,
+          ),
+        )
+      ],
+    );
+  }
 
-Widget _addTaskButton() {
-  return FloatingActionButton(
-    onPressed: () {},
-    child: const Icon(Icons.add),
-  );
+  Widget _addTaskButton() {
+    return FloatingActionButton(
+      onPressed: _displayTaskPopup,
+      child: Icon(Icons.add),
+    );
+  }
+
+  void _displayTaskPopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext _context) {
+        return AlertDialog(
+          title: const Text("Add New Task!"),
+          content: TextField(
+            onSubmitted: (_value) {},
+            onChanged: (_value) {
+              // Esta es la manera correcta de guardar un valor,
+              // cada que cambia el contenido del TexField se manda
+              //llamar la funcion build, por eso se imprime cada que se ingresa una letra
+
+              setState(() {
+                _newTaskContent = _value;
+                if (_newTaskContent == '') {
+                  _newTaskContent = null;
+                }
+              });
+            },
+          ),
+        );
+      },
+    );
+  }
 }
