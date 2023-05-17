2ee2b864bd58d3a3c1a1b93b51f6d6fc993a7793
Ricardo Herrera <72674229+ricardoherreral@users.noreply.github.com>
2023-05-16 18:24:42 -0600

    Inicializando Hive y describiendo como funciona el Future en flutter

--- taskly/lib/main.dart
+++ taskly/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:taskly/pages/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  //Inicializando la herramienta para la base de datos,
+  //regresa un metodo futuro, por eso se utiliza el async await
+  await Hive.initFlutter("hive_boxes");
   runApp(const MyApp());
 }
 
